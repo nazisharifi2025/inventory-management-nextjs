8c262732fa560c0395a70fe7c932ff2c0a37e2d5
nazi sharifi <banonazisharifi@gmail.com>
2026-08-15 22:57:36 +0430

    add new commit in new branch

--- src/app/Dashboard/Total Products.tsx
+++ src/app/Dashboard/Total Products.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function TotalProducts() {
   return (
-    <div>TotalProducts</div>
+    <div className=' p-4 rounded-md bg-gray-300/40 border border-gray-700  flex justify-center items-center gap-2'>TotalProducts</div>
   )
 }
 
