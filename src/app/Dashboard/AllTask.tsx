d1aeaf75e7920c69868419be340381f4ee5a5590
nazi sharifi <banonazisharifi@gmail.com>
2026-08-20 23:55:50 +0430

    add new commit

--- src/app/Dashboard/AllTask.tsx
+++ src/app/Dashboard/AllTask.tsx
@@ -4,6 +4,7 @@ function AllTask() {
   return (
     <div className=' w-full h-screen flex justify-center items-center'>
       <div className=" border rounded-md border-gray-600 bg-stone-200/40"></div>
+      <div className=" border rounded-md border-gray-600 bg-stone-200/40"></div>
     </div>
   )
 }
