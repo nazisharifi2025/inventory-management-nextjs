763bc64eada6d8b3b10d9d0d69a6f2d016e24316
nazi sharifi <banonazisharifi@gmail.com>
2026-09-02 10:37:16 +0430

    add new commit

---
+++ src/app/componnent/Div.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Div() {
+  return (
+    <div className=' h-screen w-full bg-gray-300 flex justify-center items-center'>This new componnent</div>
+  )
+}
+
+export default Div
