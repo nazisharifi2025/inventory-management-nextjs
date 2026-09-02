d43e037b6082d1be94b2fe18d3f009400263ac27
nazi sharifi <banonazisharifi@gmail.com>
2026-09-02 10:16:42 +0430

    marge and confleg

--- src/app/Dashboard/AllTask.tsx
+++ src/app/Dashboard/AllTask.tsx
@@ -5,7 +5,7 @@ function AllTask() {
     <div className=' w-full h-screen flex justify-center items-center'>
       <div className=" border rounded-md border-gray-600 bg-stone-200/40"></div>
       <div className=" border rounded-md border-gray-600 bg-stone-200/40"></div>
-      <div className=" border rounded-md border-gray-600 bg-stone-200/40">New Task</div>
+      <div className=" border rounded-md border-gray-600 bg-stone-200/40">New Task in this feature</div>
     </div>
   )
 }
