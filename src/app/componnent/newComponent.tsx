4648794aa64aa02efd57205d41a2f92f74da0b51
nazi sharifi <banonazisharifi@gmail.com>
2026-09-06 23:29:08 +0430

    add new div

--- src/app/componnent/newComponent.tsx
+++ src/app/componnent/newComponent.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function newComponent() {
   return (
-    <div className=" flex items center h-screen w-full bg-gray-400">newComponent</div>
+    <div className=" flex items center h-screen w-full bg-gray-400">
+      <div className=" bg-blue-200/30 text-white font-boald text-2xl flex justyfi-center items-center "></div>
+    </div>
   )
 }
 
