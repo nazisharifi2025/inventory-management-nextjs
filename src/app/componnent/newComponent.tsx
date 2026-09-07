29767fe94278e70d82f69975d926d326000ce486
nazi sharifi <banonazisharifi@gmail.com>
2026-09-07 23:48:37 +0430

    add new div in new componennt

--- src/app/componnent/newComponent.tsx
+++ src/app/componnent/newComponent.tsx
@@ -4,6 +4,7 @@ function newComponent() {
   return (
     <div className=" flex items center h-screen w-full bg-gray-400">
       <div className=" bg-blue-200/30 text-white font-boald text-2xl flex justyfi-center items-center "></div>
+      <div className=" bg-blue-200/30 text-white font-boald text-2xl flex justyfi-center items-center "></div>
     </div>
   )
 }
