06fe16539a10d4bcafe4355397dd6b28a139dffa
nazi sharifi <banonazisharifi@gmail.com>
2026-09-08 23:52:53 +0430

    add new dev

--- src/app/componnent/newComponent.tsx
+++ src/app/componnent/newComponent.tsx
@@ -5,6 +5,7 @@ function newComponent() {
     <div className=" flex items center h-screen w-full bg-gray-400">
       <div className=" bg-blue-200/30 text-white font-boald text-2xl flex justyfi-center items-center "></div>
       <div className=" bg-blue-200/30 text-white font-boald text-2xl flex justyfi-center items-center "></div>
+      <div className=" bg-blue-200/30 text-white font-boald text-2xl flex justyfi-center items-center "></div>
     </div>
   )
 }
