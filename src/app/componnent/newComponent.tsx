314e9f15886d08074ffc290c40f1b42ada677d48
nazi sharifi <banonazisharifi@gmail.com>
2026-09-22 23:54:31 +0430

    add a text

--- src/app/componnent/newComponent.tsx
+++ src/app/componnent/newComponent.tsx
@@ -12,7 +12,9 @@ function newComponent() {
       <div className=" bg-blue-200/30 text-white font-boald text-2xl flex justyfi-center items-center "></div>
       <div className=" bg-blue-200/30 text-white font-boald text-2xl flex justyfi-center items-center "></div>
       <div className=" bg-blue-200/30 text-white font-boald text-2xl flex justyfi-center items-center "></div>
-      <div className=" bg-blue-200/30 text-white font-boald text-2xl flex justyfi-center items-center "></div>
+      <div className=" bg-blue-200/30 text-white font-boald text-2xl flex justyfi-center items-center ">
+      This is a emptey div for git hub
+      </div>
     </div>
   )
 }
