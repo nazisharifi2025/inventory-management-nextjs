5c40f3e37f315095a97c16c68d93f88772265d10
nazi sharifi <banonazisharifi@gmail.com>
2026-09-24 21:08:25 +0430

    add new comment

--- src/app/componnent/newComponent.tsx
+++ src/app/componnent/newComponent.tsx
@@ -18,6 +18,9 @@ function newComponent() {
       <div className=" bg-blue-200/30 text-white font-boald text-2xl flex justyfi-center items-center ">
       This is a emptey div for git hub
       </div>
+      <div className=" bg-blue-200/30 text-white font-boald text-2xl flex justyfi-center items-center ">
+      This is a emptey div for git hub
+      </div>
     </div>
   )
 }
